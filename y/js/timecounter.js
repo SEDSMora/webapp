0247983c415217bbef7444152992c8ad2701cf97
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2021-12-17 05:31:50 +0530

    nova submission

--- y/js/timecounter.js
+++ y/js/timecounter.js
@@ -4,17 +4,17 @@ const second = 1000,
   day = hour * 24;
 
 var counterState = "collapsed";
-let countDown = new Date("Oct 16 , 2021 18:55:00").getTime(),
+let countDown = new Date("Dec 26 , 2021 23:59:00").getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    // (document.getElementById("days").innerText = Math.floor(
-    //   distance / (1000 * 60 * 60 * 24)
-    // )),
-    (document.getElementById("hours").innerText = Math.floor(
-      (distance % day) / hour
+    (document.getElementById("days").innerText = Math.floor(
+      distance / (1000 * 60 * 60 * 24)
     )),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      )),
       (document.getElementById("minutes").innerText = Math.floor(
         (distance % hour) / minute
       )),
@@ -24,12 +24,15 @@ let countDown = new Date("Oct 16 , 2021 18:55:00").getTime(),
 
     //do something later when date is reached
     if (distance < 0) {
-      (document.getElementById("hours").innerText = 00),
+      (document.getElementById("days").innerText = 00),
         (document.getElementById("hours").innerText = 00),
         (document.getElementById("minutes").innerText = 00),
         (document.getElementById("seconds").innerText = 00);
-      document.getElementById("link-text").innerText = "JOIN NOW";
-      document.getElementById("button-link").disabled = false;
+      document.getElementById("link-text").innerText =
+        "Video Submission Period is over";
+
+      document.getElementById("button-link").disabled = true;
+
       // document.getElementById("button-link").style.visibility = "visible";
       if (distance >= -1000) {
         var myCollapse = document.getElementById("time-counter");
@@ -41,10 +44,8 @@ let countDown = new Date("Oct 16 , 2021 18:55:00").getTime(),
       }
       clearInterval(x);
     } else {
-      document.getElementById("button-link").disabled = true;
-      document.getElementById("link-text").innerText =
-        "Please wait... \n Link will be available @ 6.55 PM";
-
+      document.getElementById("button-link").disabled = false;
+      document.getElementById("link-text").innerText = "Submit Your Video Here";
       // document.getElementById("button-link").style.visibility = "hidden";
 
       if (counterState == "collapsed") {
@@ -52,13 +53,10 @@ let countDown = new Date("Oct 16 , 2021 18:55:00").getTime(),
         var bsCollapse = new bootstrap.Collapse(myCollapse, {
           toggle: true,
         });
+
         counterState = "show";
       } else {
         //do nothing
       }
     }
   }, second);
-
-function link() {
-  window.open("https://udenver.zoom.us/j/81582596274", "_blank");
-}
